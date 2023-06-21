42465bfd785aa1a33f376115a84f75dcf0fc596c
Oleh Shliazhko <olehshliazhko@gmail.com>
2023-06-21 21:44:01 +0000

    fix deps

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python==0.1.65 --force-reinstall --upgrade --no-cache-dir
 mkdir -p wiki_mpnet_index/data/en/
 echo "Downloading wiki index..."
 wget https://huggingface.co/datasets/olmer/wiki_mpnet_index/resolve/main/data/en/embs_IVF16384_HNSW32_2lvl_full.idx -P ./wiki_mpnet_index/data/en/
